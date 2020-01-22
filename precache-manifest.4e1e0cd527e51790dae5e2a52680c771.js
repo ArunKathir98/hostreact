406e6cbde2a706c787edb314d720b22129ee366a
Arun kumar kathiresan <arun.kathiresan@aspiresys.com>
2020-01-22 10:22:11 +0530

    Updates

---
+++ precache-manifest.4e1e0cd527e51790dae5e2a52680c771.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20249b665b4d1fd63e0c13860a4c11e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "641703882a64e55c8372",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cd6bac3399923e691f9e",
+    "url": "/static/js/2.48b98873.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.48b98873.chunk.js.LICENSE"
+  },
+  {
+    "revision": "641703882a64e55c8372",
+    "url": "/static/js/main.d06ad745.chunk.js"
+  },
+  {
+    "revision": "4f8b734a7e80109b6f7e",
+    "url": "/static/js/runtime-main.10a268e8.js"
+  },
+  {
+    "revision": "113945106775b0c5930d06cee62dd0f8",
+    "url": "/static/media/logo.11394510.svg"
+  }
+]);
